7e8d60e3d2ff16b3d4380bb72f0c95fa2fa76deb
Alex Lalejini <amlalejini@gmail.com>
2018-11-20 15:06:22 -0500

    Update run avida analysis script to pull out sequences at given generation. Update avida lineage dat to seq script to run over multiple dat files.

--- source/scripts/avidaLineageDatToSeqs.py
+++ source/scripts/avidaLineageDatToSeqs.py
@@ -76,98 +76,99 @@ def main():
             run_dir = os.path.join(data_directory, treatment, run)
             # Grab the .dat lineage file
             dats = [f for f in os.listdir(run_dir) if "lineage_details-" in f]
-            dat_fpath = os.path.join(run_dir, dats[0])
-            # Parse the detail file
-            details = ParseDetailFile(dat_fpath)
-            # Max update
-            max_update = dat_fpath.split("-")[-1].split(".")[0]
-            # Extract phenotype sequence
-            phenotype_seq_states = []
-            phenotype_seq_starts = []
-            phenotype_seq_durations = []
-            phenotype_seq_volatility = 0 # Number of state changes   
-            sub_mut_cnt = 0
-            ins_mut_cnt = 0
-            dels_mut_cnt = 0
-            for i in range(0, len(details)):
-                muts_from_parent = details[i]["mutations from parent"].split(",")
-                for mut in muts_from_parent:
-                    if (len(mut) == 0): continue
-                    if (mut[0] == "M"): sub_mut_cnt += 1
-                    elif (mut[0] == "I"): ins_mut_cnt += 1
-                    elif (mut[0] == "D"): dels_mut_cnt += 1
-                    else: print("Unknown mutation type (" + str(mut) + ")!")
-                # State?
-                state = "-".join([task.upper() for task in tasks if details[i][task] == "1"])
-                if (i > 0):
-                    if (phenotype_seq_states[-1] != state):
-                        phenotype_seq_volatility += 1                
-                # Start?
-                start = int(details[i]["update born"])
-                if start < 0: start = 0 # Clamp start update at 0 for sanity
-                # Update previous duration
-                if i: phenotype_seq_durations.append(start - phenotype_seq_starts[-1])
-                phenotype_seq_starts.append(start)
-                phenotype_seq_states.append(state)
-            phenotype_seq_durations.append(int(max_update) - phenotype_seq_starts[-1])
-            total_muts = sub_mut_cnt + ins_mut_cnt + dels_mut_cnt
+            for dat in dats:
+                dat_fpath = os.path.join(run_dir, dat)
+                # Parse the detail file
+                details = ParseDetailFile(dat_fpath)
+                # Max update
+                max_update = dat_fpath.split("-")[-1].split(".")[0]
+                # Extract phenotype sequence
+                phenotype_seq_states = []
+                phenotype_seq_starts = []
+                phenotype_seq_durations = []
+                phenotype_seq_volatility = 0 # Number of state changes   
+                sub_mut_cnt = 0
+                ins_mut_cnt = 0
+                dels_mut_cnt = 0
+                for i in range(0, len(details)):
+                    muts_from_parent = details[i]["mutations from parent"].split(",")
+                    for mut in muts_from_parent:
+                        if (len(mut) == 0): continue
+                        if (mut[0] == "M"): sub_mut_cnt += 1
+                        elif (mut[0] == "I"): ins_mut_cnt += 1
+                        elif (mut[0] == "D"): dels_mut_cnt += 1
+                        else: print("Unknown mutation type (" + str(mut) + ")!")
+                    # State?
+                    state = "-".join([task.upper() for task in tasks if details[i][task] == "1"])
+                    if (i > 0):
+                        if (phenotype_seq_states[-1] != state):
+                            phenotype_seq_volatility += 1                
+                    # Start?
+                    start = int(details[i]["update born"])
+                    if start < 0: start = 0 # Clamp start update at 0 for sanity
+                    # Update previous duration
+                    if i: phenotype_seq_durations.append(start - phenotype_seq_starts[-1])
+                    phenotype_seq_starts.append(start)
+                    phenotype_seq_states.append(state)
+                phenotype_seq_durations.append(int(max_update) - phenotype_seq_starts[-1])
+                total_muts = sub_mut_cnt + ins_mut_cnt + dels_mut_cnt
 
-            phenotype_seq_unique_state_cnt = len(set(phenotype_seq_states))
-            phenotype_seq_length = len(phenotype_seq_states)
-            if (phenotype_seq_volatility):
-                phenotype_seq_chg_rate = sum(phenotype_seq_durations) / phenotype_seq_volatility
-            else:
-                phenotype_seq_chg_rate = 0
-            
-            # Compress phenotype sequence
-            compressed__phenotype_seq_starts = []
-            compressed__phenotype_seq_states = []
-            compressed__phenotype_seq_durations = []
-            compressed__phenotype_seq_volatility = 0
-            for i in range(0, len(phenotype_seq_states)):
-                # If we're at the first state, just set start, states, and duration from source.
-                if (i == 0):
-                    compressed__phenotype_seq_starts.append(phenotype_seq_starts[0])
-                    compressed__phenotype_seq_states.append(phenotype_seq_states[0])
-                    compressed__phenotype_seq_durations.append(phenotype_seq_durations[0])
-                    continue
-                # Are we the same?
-                if (phenotype_seq_states[i] == compressed__phenotype_seq_states[-1]):
-                    # Increment duration
-                    compressed__phenotype_seq_durations[-1]+=phenotype_seq_durations[i]
-                    continue
-                else: # Are we different?
-                    # Different!
-                    compressed__phenotype_seq_starts.append(phenotype_seq_starts[i])
-                    compressed__phenotype_seq_states.append(phenotype_seq_states[i])
-                    compressed__phenotype_seq_durations.append(phenotype_seq_durations[i])
-                    compressed__phenotype_seq_volatility += 1
-            
-            compressed__phenotype_seq_unique_state_cnt = len(set(compressed__phenotype_seq_states))
-            compressed__phenotype_seq_length = len(compressed__phenotype_seq_states)
-            if (compressed__phenotype_seq_volatility):
-                compressed__phenotype_seq_chg_rate = sum(compressed__phenotype_seq_durations) / compressed__phenotype_seq_volatility
-            else:
-                compressed__phenotype_seq_chg_rate = 0
-            
-            # Write line of content!
-            # "treatment,run_id,max_update,total_muts,total_substitutions,total_insertions,total_deletions,"
-            #  phen_seq_by_geno_unique_state_cnt, phen_seq_by_geno_length, phen_seq_by_geno_volatility, phen_seq_by_geno_chg_rate
-            # "phen_seq_by_geno_state,phen_seq_by_geno_start,phen_seq_by_geno_duration"
-            #  phen_seq_by_phen_unique_state_cnt, phen_seq_by_phen_length, phen_seq_by_phen_volatility, phen_seq_by_phen_chg_rate
-            # "phen_seq_by_phen_state,phen_seq_by_phen_start,phen_seq_by_phen_duration\n"
-            phen_seq_by_geno_state = "\"{}\"".format(",".join(phenotype_seq_states))
-            phen_seq_by_geno_start = "\"{}\"".format(",".join(map(str, phenotype_seq_starts)))
-            phen_seq_by_geno_duration = "\"{}\"".format(",".join(map(str, phenotype_seq_durations)))
-            phen_seq_by_phen_state = "\"{}\"".format(",".join(compressed__phenotype_seq_states))
-            phen_seq_by_phen_start = "\"{}\"".format(",".join(map(str, compressed__phenotype_seq_starts)))
-            phen_seq_by_phen_duration = "\"{}\"".format(",".join(map(str, compressed__phenotype_seq_durations)))
-            
-            lineage_seq_content += ",".join(map(str, [treatment, run, max_update, total_muts, sub_mut_cnt, ins_mut_cnt, dels_mut_cnt, 
-                                                      phenotype_seq_unique_state_cnt,phenotype_seq_length,phenotype_seq_volatility,phenotype_seq_chg_rate,
-                                                      phen_seq_by_geno_state, phen_seq_by_geno_start, phen_seq_by_geno_duration, 
-                                                      compressed__phenotype_seq_unique_state_cnt,compressed__phenotype_seq_length,compressed__phenotype_seq_volatility,compressed__phenotype_seq_chg_rate,
-                                                      phen_seq_by_phen_state, phen_seq_by_phen_start, phen_seq_by_phen_duration])) + "\n"
+                phenotype_seq_unique_state_cnt = len(set(phenotype_seq_states))
+                phenotype_seq_length = len(phenotype_seq_states)
+                if (phenotype_seq_volatility):
+                    phenotype_seq_chg_rate = sum(phenotype_seq_durations) / phenotype_seq_volatility
+                else:
+                    phenotype_seq_chg_rate = 0
+                
+                # Compress phenotype sequence
+                compressed__phenotype_seq_starts = []
+                compressed__phenotype_seq_states = []
+                compressed__phenotype_seq_durations = []
+                compressed__phenotype_seq_volatility = 0
+                for i in range(0, len(phenotype_seq_states)):
+                    # If we're at the first state, just set start, states, and duration from source.
+                    if (i == 0):
+                        compressed__phenotype_seq_starts.append(phenotype_seq_starts[0])
+                        compressed__phenotype_seq_states.append(phenotype_seq_states[0])
+                        compressed__phenotype_seq_durations.append(phenotype_seq_durations[0])
+                        continue
+                    # Are we the same?
+                    if (phenotype_seq_states[i] == compressed__phenotype_seq_states[-1]):
+                        # Increment duration
+                        compressed__phenotype_seq_durations[-1]+=phenotype_seq_durations[i]
+                        continue
+                    else: # Are we different?
+                        # Different!
+                        compressed__phenotype_seq_starts.append(phenotype_seq_starts[i])
+                        compressed__phenotype_seq_states.append(phenotype_seq_states[i])
+                        compressed__phenotype_seq_durations.append(phenotype_seq_durations[i])
+                        compressed__phenotype_seq_volatility += 1
+                
+                compressed__phenotype_seq_unique_state_cnt = len(set(compressed__phenotype_seq_states))
+                compressed__phenotype_seq_length = len(compressed__phenotype_seq_states)
+                if (compressed__phenotype_seq_volatility):
+                    compressed__phenotype_seq_chg_rate = sum(compressed__phenotype_seq_durations) / compressed__phenotype_seq_volatility
+                else:
+                    compressed__phenotype_seq_chg_rate = 0
+                
+                # Write line of content!
+                # "treatment,run_id,max_update,total_muts,total_substitutions,total_insertions,total_deletions,"
+                #  phen_seq_by_geno_unique_state_cnt, phen_seq_by_geno_length, phen_seq_by_geno_volatility, phen_seq_by_geno_chg_rate
+                # "phen_seq_by_geno_state,phen_seq_by_geno_start,phen_seq_by_geno_duration"
+                #  phen_seq_by_phen_unique_state_cnt, phen_seq_by_phen_length, phen_seq_by_phen_volatility, phen_seq_by_phen_chg_rate
+                # "phen_seq_by_phen_state,phen_seq_by_phen_start,phen_seq_by_phen_duration\n"
+                phen_seq_by_geno_state = "\"{}\"".format(",".join(phenotype_seq_states))
+                phen_seq_by_geno_start = "\"{}\"".format(",".join(map(str, phenotype_seq_starts)))
+                phen_seq_by_geno_duration = "\"{}\"".format(",".join(map(str, phenotype_seq_durations)))
+                phen_seq_by_phen_state = "\"{}\"".format(",".join(compressed__phenotype_seq_states))
+                phen_seq_by_phen_start = "\"{}\"".format(",".join(map(str, compressed__phenotype_seq_starts)))
+                phen_seq_by_phen_duration = "\"{}\"".format(",".join(map(str, compressed__phenotype_seq_durations)))
+                
+                lineage_seq_content += ",".join(map(str, [treatment, run, max_update, total_muts, sub_mut_cnt, ins_mut_cnt, dels_mut_cnt, 
+                                                        phenotype_seq_unique_state_cnt,phenotype_seq_length,phenotype_seq_volatility,phenotype_seq_chg_rate,
+                                                        phen_seq_by_geno_state, phen_seq_by_geno_start, phen_seq_by_geno_duration, 
+                                                        compressed__phenotype_seq_unique_state_cnt,compressed__phenotype_seq_length,compressed__phenotype_seq_volatility,compressed__phenotype_seq_chg_rate,
+                                                        phen_seq_by_phen_state, phen_seq_by_phen_start, phen_seq_by_phen_duration])) + "\n"
 
     # Write out sequences to file
     with open("lineage_sequences.csv", "w") as fp:
